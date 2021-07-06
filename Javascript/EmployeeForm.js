06fa8cba5e8d5fd7e7836b6b52b6aeb7ad89d091
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-07-06 23:49:56 +0530

    Add-add reset button functionality

--- Javascript/EmployeeForm.js
+++ Javascript/EmployeeForm.js
@@ -82,7 +82,29 @@ function createAndUpdateStorage(employeePayrollData) {
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
-const setTextValue = (id,value) => {
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2021');
+    setValue('#notes','');
+
+
+} 
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+
+}
+
+const setValue = (id,value) => {
     const element = document.querySelector(id);
     element.value = value;
 }
