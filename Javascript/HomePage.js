d43666655dbd06b0961ede2e7225700c02ba4b0a
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-07-13 23:42:44 +0530

    Add-convert application from stand alone to client-server architecture, now data communication is happening from json server

--- Javascript/HomePage.js
+++ Javascript/HomePage.js
@@ -1,16 +1,37 @@
 let empPayrollList;
 window.addEventListener('DOMContentLoaded',(event) => {
-    empPayrollList = getEmployeePayrollDataFromStorage();
+    if(site_properties.use_local_storage.match("true")){
+        getEmployeePayrollDataFromStorage();
+    }else
+    {
+        getEmployeePayrollDataFromServer();
+    }
+});
+
+const processEmployeePayrollDataResponse = () => {
     document.querySelector('.emp-count').textContent = empPayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
-});
-
-const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ?
-                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
 
+const getEmployeePayrollDataFromStorage = () => {
+    empPayrollList = localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    processEmployeePayrollDataResponse();
+}
+
+const getEmployeePayrollDataFromServer = () => {
+    makeServiceCall("GET",site_properties.server_url,true)
+    .then(responseText => {
+        empPayrollList = JSON.parse(responseText);
+        processEmployeePayrollDataResponse();
+    })
+    .catch(error => {
+        console.log("GET Error Status "+JSON.stringify(error));
+        empPayrollList = [];
+        processEmployeePayrollDataResponse();
+    });
+}
 const createInnerHtml = () => {
     if (empPayrollList.length == 0) return;
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
@@ -25,8 +46,8 @@ const createInnerHtml = () => {
           <td>${empPayrollData._salary}</td>
           <td>${stringifyDate(empPayrollData._startDate)}</td>
           <td>
-           <img id="${empPayrollData.id}" src="../assets/delete-black-18dp.svg" onclick="remove(this)" alt="delete">
-           <img id="${empPayrollData.id}" src="../assets/create-black-18dp.svg" onclick="update(this)" alt="edit">
+           <img class="delete-button" id="${empPayrollData.id}" src="../assets/delete-black-18dp.svg" onclick="remove(this)" alt="delete">
+           <img class="update-button" id="${empPayrollData.id}" src="../assets/create-black-18dp.svg" onclick="update(this)" alt="edit">
           </td>
         </tr>
        `;
@@ -40,10 +61,27 @@ const remove = (node) => {
     const index = empPayrollList.map(empData => empData.id)
                                 .indexOf(empPayrollData.id);
     empPayrollList.splice(index,1);
-    localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
-    document.querySelector(".emp-count").textContent = empPayrollList.length();
-    createInnerHtml();
+    if(site_properties.use_local_storage.match("true")){
+        localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
+        createInnerHtml();
+    }else{
+        const deleteURL = site_properties.server_url+"/"+empPayrollData.id.toString();
+        makeServiceCall("DELETE",deleteURL,false)
+         .then(responseText => {
+             createInnerHtml();
+         })
+         .catch(error => {
+             console.log("DELETE Error Status :"+JSON.stringify(error));
+         });
+    }
 
+}
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
+    if(!empPayrollData) return;
+    localStorage.setItem('editEmp',JSON.stringify(empPayrollData));
+    window.location.replace(site_properties.add_emp_payroll_form);
 }
 
 
--- Javascript/siteProperties.js
+++ Javascript/siteProperties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../Pages/HomePage.html",
     add_emp_payroll_form: "../Pages/EmployeePayrollForm.html",
     server_url:"http://localhost:3000/employee"
